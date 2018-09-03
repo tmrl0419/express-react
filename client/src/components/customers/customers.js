d109ea6970939d939bafcee6be10df89d70c3293
tmrl0419 <them0419@naver.com>
2018-09-03 20:06:11 +0900

    First commit

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+    constructor(){
+        super();
+        this.state = {
+            customers : []
+        }
+    }
+    
+    componentDidMount(){
+        fetch('/api/customers')
+            .then(res => res.json())
+            .then(customers => this.setState({customers}, ()=> console.log('Customers Fetched..', customers)));
+    }
+
+    render() {
+        return (
+        <div>
+          <h2>Customers</h2>
+        </div>
+        );
+    }
+}
+
+export default Customers;
