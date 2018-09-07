aa9ed281f8cdffeae4cb8b1c4a15c29cbac597ee
them0419 <them0419@naver.com>
2018-09-07 20:32:36 +0900

    debugged

--- auth/index.js
+++ auth/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express  = require('express');
 const bcrypt = require('bcrypt');
 const  router = express.Router();
@@ -7,11 +6,6 @@ const User = require('../db/user');
 
 
 
-=======
-const  express  = require('express');
-const  router = express.Router();
-
->>>>>>> 354936181f9fec55927ae77519721f42d65d6f92
 // Route paths are prepended with /auth
 
 router.get('/',(req,res)=>{
@@ -33,7 +27,6 @@ function validateUser(user){
 
 router.post('/signup',(req,res, next)=>{
     if(validateUser(req.body)){
-<<<<<<< HEAD
         User
             .getOne(req.body.user_id)
             .then(user =>{
@@ -65,11 +58,6 @@ router.post('/signup',(req,res, next)=>{
                     next(new Error('Email in use'))
                 }
             });
-=======
-        res.json({
-            message:'CHECK'
-        })
->>>>>>> 354936181f9fec55927ae77519721f42d65d6f92
     } else{
         next(new Error('Invalid user'));
     }
--- server.js
+++ server.js
@@ -3,15 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-<<<<<<< HEAD
 var {Client} = require('pg');
 
-=======
-var bodyParser = require('body-parser');
-var {Client} = require('pg');
-
-
->>>>>>> 354936181f9fec55927ae77519721f42d65d6f92
 var dbconfig = require('./db/dbconfig');
 var  auth  =  require('./auth/index');
 
@@ -24,23 +17,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-<<<<<<< HEAD
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-=======
-const client = new Client(dbconfig);
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, 'public')));
->>>>>>> 354936181f9fec55927ae77519721f42d65d6f92
 
 app.use('/auth',auth);
 
-client.connect();
 
 app.get('/api/customers', (req, res)=>{
     const customers = [
@@ -53,31 +34,15 @@ app.get('/api/customers', (req, res)=>{
 });
 
 
-<<<<<<< HEAD
 app.get('/chk', (req, res)=>{
 
     client.query('SELECT * FROM users')
-=======
-app.post('/regist', (req, res)=>{
-    var user_id = req.body.user_id,
-        password = req.body.password,
-        name = req.body.name;
-
-    const text = 'INSERT INTO public.users (user_id,password,name) VALUES($1,$2,$3)'
-    const values = [user_id , password ,name];
-
-    client.query(text,values)
->>>>>>> 354936181f9fec55927ae77519721f42d65d6f92
         .then(res => {
             console.log(res.rows);
             client.end();
         })
         .catch(e => console.error(e.stack)
-<<<<<<< HEAD
         )
-=======
-    )
->>>>>>> 354936181f9fec55927ae77519721f42d65d6f92
     // 이미 존재 하는 아이디 걸러네기
 });
 
